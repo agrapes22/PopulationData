b23a46722deca9be203ccd1008513b134fe6eac9
agrapes22 <agrapes22@gmail.com>
2021-06-13 16:17:57 -0700

    Initial Commit

--- PopulationData/Form1.Designer.cs
+++ PopulationData/Form1.Designer.cs
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.avgChangeText = new System.Windows.Forms.Label();
+            this.biggestIncreaseText = new System.Windows.Forms.Label();
+            this.smallestIncreaseText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Population File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Population Calculation";
+            // 
+            // avgChangeText
+            // 
+            this.avgChangeText.AutoSize = true;
+            this.avgChangeText.Location = new System.Drawing.Point(511, 169);
+            this.avgChangeText.Name = "avgChangeText";
+            this.avgChangeText.Size = new System.Drawing.Size(35, 13);
+            this.avgChangeText.TabIndex = 2;
+            this.avgChangeText.Text = "label2";
+            this.avgChangeText.Visible = false;
+            // 
+            // biggestIncreaseText
+            // 
+            this.biggestIncreaseText.AutoSize = true;
+            this.biggestIncreaseText.Location = new System.Drawing.Point(511, 197);
+            this.biggestIncreaseText.Name = "biggestIncreaseText";
+            this.biggestIncreaseText.Size = new System.Drawing.Size(35, 13);
+            this.biggestIncreaseText.TabIndex = 3;
+            this.biggestIncreaseText.Text = "label3";
+            this.biggestIncreaseText.Visible = false;
+            // 
+            // smallestIncreaseText
+            // 
+            this.smallestIncreaseText.AutoSize = true;
+            this.smallestIncreaseText.Location = new System.Drawing.Point(511, 225);
+            this.smallestIncreaseText.Name = "smallestIncreaseText";
+            this.smallestIncreaseText.Size = new System.Drawing.Size(35, 13);
+            this.smallestIncreaseText.TabIndex = 4;
+            this.smallestIncreaseText.Text = "label4";
+            this.smallestIncreaseText.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Average annual increase:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(339, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Year with greatest increase:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(339, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Year with least increase:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.smallestIncreaseText);
+            this.Controls.Add(this.biggestIncreaseText);
+            this.Controls.Add(this.avgChangeText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label avgChangeText;
+        private System.Windows.Forms.Label biggestIncreaseText;
+        private System.Windows.Forms.Label smallestIncreaseText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- PopulationData/Form1.cs
+++ PopulationData/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,125 @@ namespace PopulationData
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = "";
+            
+            DialogResult result = openFile.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string file = openFile.FileName;
+                try
+                {
+                    text = File.ReadAllText(file);
+                }
+                catch (IOException)
+                {
+
+                }
+
+                String[] pops = text.Split('\r');
+
+                int[,] population = new int[pops.Length, 2];
+
+                int start = 1950;
+
+                for (int i = 0; i < pops.Length; i++)
+                {
+                    population[i, 0] = start;
+                    population[i, 1] = int.Parse(pops[i]);
+                    
+                    start++;
+                }
+
+                int size = population.GetLength(0);
+
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write("Year: " + population[i, 0]);
+                    Console.Write(" Pop: " + population[i, 1]);
+                    Console.WriteLine();
+                  
+                }
+
+                avgChangeText.Text = "" + averageChange(population);
+                avgChangeText.Visible = true;
+
+                biggestIncreaseText.Text = "" + highestChange(population);
+                biggestIncreaseText.Visible = true;
+
+                smallestIncreaseText.Text = "" + leastChange(population);
+                smallestIncreaseText.Visible = true;
+
+            }
+
+            //average annual change in pop during time period
+            double averageChange(int[,] pop)
+            {
+                double avg = 0.0;
+
+                int count = pop.GetLength(0) - 1;
+                double sum = 0;
+
+                //get difference between each year and average it
+                for (int i = pop.GetLength(0) - 1; i > 0; i--)
+                {
+                    int diff = pop[i, 1] - pop[i - 1, 1];
+
+                    sum += diff;
+                }
+
+                avg = sum / count;
+
+                return avg;
+            }
+
+            //year with the greatest increase
+            int highestChange(int[,] pop)
+            {
+                int year = 0;
+                int highest = 0;
+
+                //loop through each year and population comparing
+                for (int i = pop.GetLength(0) - 1; i > 0; i--)
+                {
+                    int diff = pop[i, 1] - pop[i - 1, 1];
+
+                    if (diff > highest)
+                    {
+                        highest = diff;
+                        year = pop[i, 0];
+                    }
+                }
+
+                return year;
+            }
+
+            //year with the least increase
+            int leastChange(int[,] pop)
+            {
+                int year = 0;
+                //set lowest to first difference, compare every other diff to it
+                int lowest = pop[pop.GetLength(0) - 1, 1] - pop[pop.GetLength(0) - 2, 1];
+
+                //loop through each year and population comparing
+                for (int i = pop.GetLength(0) - 2; i > 0; i--)
+                {
+                    int diff = pop[i, 1] - pop[i - 1, 1];
+
+                    if (diff < lowest)
+                    {
+                        lowest = diff;
+                        year = pop[i, 0];
+                    }
+                }
+
+                return year;
+            }
+
         }
     }
 }
